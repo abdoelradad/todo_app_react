2617f4c032e0357d67f62e87d252491249eab5bd
abdoelradad <elradad2000@gmail.com>
2023-07-02 08:23:02 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     const task = {
       id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
       taskName: newTask,
+      completed: false,
     };
     const newTodoList = [...todoList, task];
     setTodoList(newTodoList);
@@ -23,6 +24,17 @@ function App() {
     setTodoList(todoList.filter((task) => task.id !== id));
   };
 
+  const completeTask = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else {
+          return task;
+        }
+      })
+    );
+  };
   return (
     <div className="App">
       <div className="addTask">
@@ -36,7 +48,9 @@ function App() {
             <Task
               taskName={task.taskName}
               id={task.id}
+              completed={task.completed}
               deleteTask={deleteTask}
+              completeTask={completeTask}
             />
           );
         })}
